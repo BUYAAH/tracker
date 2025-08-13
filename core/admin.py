5b08ae098a13b694ee12e6fc20723238145741b1
root <root@Amadeus.localdomain>
2025-08-13 10:40:42 +0200

    Layout update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'latitude', 'longitude', 'timestamp', 'accuracy', 'battery')
+    list_filter = ('user', 'timestamp')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    readonly_fields = ('timestamp',)
+    ordering = ('-timestamp',)
